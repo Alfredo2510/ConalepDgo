7c4bfa1834b73d2b07fb9cd3b0ba739806b31cd4
Alfredo.nava <alfredo_v_n@hotmail.com>
2018-08-21 19:47:12 -0500

    nueva version de header

---
+++ header.php
@@ -0,0 +1,225 @@
+<!DOCTYPE html>
+<html lang="es-mx" class="nav-no-js">
+<head> <!-- Información de la pagina para el navegador y también para enlazar archivos (css, js, entre otros) -->
+    <meta charset="UTF-8"> <!-- Decodificación de los caracteres -->
+    <title>CONALEP Durango</title>
+    <meta name="description" content="Colegio de Educación Profesional Técnica del Estado de Durango
+                                     | Portal Conalep">
+    <meta name="author" content="https://plus.google.com/109717154884126540058">
+    <meta name="owner" content="CONALEP Durango">
+    <meta name="viewport" content="width=device-width, user-scalabe=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+  
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/fonts.css">
+    <link rel="stylesheet" href="css/style index/estilo-index.css">
+    <link rel="stylesheet" href="css/style index/nav-core.css">
+    <link rel="stylesheet" href="css/style index/nav-layout.css">
+
+    <script src="js/librery/modernizr-custom.js"></script>
+    <script src="js/librery/all.min.js"></script>
+  
+   <!--  <script src="js/main-movil.js"></script> -->
+</head>
+
+<body> 
+        <div id="up-header">
+                <a class="info-up" id="tel-head" href="tel:+52-618-819-9626"> <span><i class="fas fa-phone icon-up"></i>(618) 819-96-26 y 817-12-68</span></a>
+                <a class="info-up" target="__blank" href="https://www.google.com.mx/maps/dir/''/conalep+durango/@24.0692589,-104.6045421,18.05z/data=!4m8!4m7!1m0!1m5!1m1!1s0x869bb6f1733f6e9f:0x1758bea35d7db179!2m2!1d-104.6038647!2d24.0698149"><span class="info-up"><i class="fas fa-map-marker-alt icon-up"></i>Selenio Lote 1, Cd Industrial, 34208 Durango, Dgo.</span></a> 
+                <a class="info-up" id="mail-head" href="mailto:direccionconalepdgo@gmail.com"> <span><i class="fas fa-envelope icon-up"></i>direccionconalepdgo@gmail.com</span></a>
+            <div id="red-icons">
+                <a target="__blank" href="https://www.facebook.com/conalep.planteldurango/"><i class="i-fb fab fa-facebook icon-upfb"></i></a> 
+                <a target="__blank" href="https://twitter.com/conalep130?lang=es"><i class="fab fa-twitter-square icon-uptw"></i></a> 
+            </div>
+        </div>
+
+
+      <!--  <h1 id="header-phone">
+            <div>
+            <a href="index.html"><img src="img/logo.png" alt="Conalep Durango"></a><i class="i-menu fas fa-bars"></i>
+            </div>
+        </h1> -->
+<header>
+        <div class="phone-c">
+            <a href="#"><img src="img/logo.png" alt="Conalep Durango"></a>
+        </div>
+</header>
+
+<a href="#" class="nav-button">Menu</a>
+     
+        <div class="master">   
+        <h1>
+            <div>
+            <a href="#"><img src="img/logo.png" alt="Conalep Durango"></a>
+            </div>
+        </h1> 
+                    <nav class="nav" > <!-- Menú de navegación -->
+                            <ul> <!-- Una lista no ordenada, para contener los elementos del menú -->
+                                <li><a href="#" ><i class="fas fa-home"></i></a></li> 
+
+                                <li  class="nav-submenu">
+                                    <a href="#" >QUIÉNES SOMOS</a>
+                                    <ul>
+                                        <li><a href="#">Misión y Visión</a></li>
+                                        <li><a href="#">Nuestros Valores</a></li>
+                                        <li><a href="#">Políticas de Calidad</a></li>
+                                        <li><a href="#">Directorio</a></li>
+                                    </ul>
+                                </li>
+                                <li class="nav-submenu">
+                                    <a href="#" >ALUMNOS</a>
+                                    <ul>
+                                        <li><a href="#">Oferta Educativa</a></li>
+                                        <li><a href="#">Red Académica</a></li>
+                                        <li class="nav-submenu"><a href="#">Becas Conalep</a>
+                                            <ul>
+                                                <li><a href="#">Convocatorias</a></li>
+                                                <li><a href="#">Padrones</a></li>
+                                                <li><a href="#">Contraloria Social</a></li>
+                                            </ul>
+                                        </li>
+                                        <li><a href="#">Valoracion de Aprovechamiento</a></li>
+                                        <li><a href="#">Becas Externas y Estímulos</a></li>
+                                        <li><a href="#">Convocatorias</a></li>
+                                    </ul>
+                                </li> <!-- Manda a un identificador marcado con la misma id  -->
+                                <li class="nav-submenu">
+                                    <a href="#">DOCENTES</a>
+                                    <ul>
+                                        <li><a href="#">Programa "Evaluación Integral"</a></li>
+                                        <li><a href="#">Formación Docente</a></li>
+                                        <li><a href="#">SEMS-COSDAC</a></li>
+                                        <li><a href="#">Convocatorias</a></li>
+                                    </ul>
+                                </li>
+                                <li class="nav-submenu">
+                                    <a href="#">VINCULACIÓN</a>
+                                    <ul>
+                                        <li><a href="#">Encuestas</a></li>
+                                        <li><a href="#">Colocación de Egresados</a></li>
+                                        <li><a href="#">Bolsa de Trabajo</a></li>
+                                        <li><a href="#">Comités de Vinculación</a></li>
+                                    </ul>
+                                </li>
+                                <li class="nav-submenu">
+                                    <a href="#">SERVICIOS</a>
+                                    <ul>
+                                        <li><a href="#">Capacitación</a></li>
+                                        <li><a href="#">Centros de Evaluación</a></li>
+                                        <li><a href="#">Atención a la Comunidad</a></li>
+                                    </ul>
+                                </li>
+                                <li class="nav-submenu">
+                                    <a href="#">TRANSPARENCIA</a>
+                                    <ul>
+                                        <li><a href="#">Aviso de Privacidad</a></li>
+                                        <li><a href="#">Cumplimiento Art 65</a></li>
+                                        <li><a href="#">Cumplimiento Art 75 LGCG</a></li>
+                                        <li><a href="#">Transparencia Presupuestal</a></li>
+                                    </ul>
+                                </li>
+                                
+                                <li> <a class="mn-mother" href="#">CONTACTO</a> </li>
+                            </ul>
+                    </nav>
+            </div>   
+
+
+
+
+    <div>
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam auctor accumsan enim, id finibus sapien imperdiet
+        a. Ut laoreet imperdiet diam quis tincidunt. Fusce risus est, suscipit ut turpis id, vehicula sodales libero.
+        Nulla a elementum risus. Morbi fringilla, ligula ac pretium bibendum, felis erat consequat nunc, in blandit
+        velit quam eu eros. Nam convallis in sem eu mollis. Mauris venenatis magna vulputate odio aliquet, sit amet
+        dapibus tortor luctus. In mauris massa, aliquet nec nisl ut, feugiat tincidunt dolor. Mauris tristique felis eu
+        tempus vestibulum. Nulla fringilla molestie consequat. Pellentesque eget porta urna.
+    </p>
+
+    <p>
+        Sed volutpat viverra diam pellentesque dictum. Nulla mollis est non est accumsan, vel viverra dolor aliquet.
+        Fusce posuere mi vel elit vulputate, nec malesuada libero porta. Aliquam nisi arcu, congue luctus massa id,
+        imperdiet facilisis turpis. Suspendisse consequat, est sit amet venenatis consectetur, massa enim lobortis quam,
+        ut viverra tortor neque non justo. Curabitur porttitor eget ante eget euismod. Duis lacinia quam in tempor
+        gravida. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nunc
+        ullamcorper accumsan nunc, feugiat gravida ex finibus in. Aenean mi felis, lacinia eget feugiat vel, maximus ac
+        mauris. Suspendisse elementum ut neque id varius. Etiam vitae ex nulla. Vestibulum blandit arcu et neque
+        pharetra, vel pulvinar lacus commodo. Donec eu sodales dolor, in placerat lorem.
+    </p>
+
+    <p>
+        Sed suscipit orci neque, luctus auctor metus dictum eget. Aliquam ante odio, posuere a nunc ut, eleifend
+        efficitur diam. Aliquam erat est, dignissim vel accumsan in, fringilla hendrerit nulla. Nam sit amet sagittis
+        sapien. Mauris tempor libero varius, volutpat mi vitae, accumsan lorem. Morbi non est porta ligula dapibus
+        fringilla. Mauris mauris est, mattis vitae purus at, posuere luctus nibh. Suspendisse lobortis justo elit, vel
+        feugiat lorem pulvinar ut. Sed molestie, magna sed luctus tempor, est felis porttitor massa, in convallis lectus
+        enim sit amet lacus. Sed quis magna vel est eleifend sollicitudin. Ut a eros lobortis, luctus nunc id, sagittis
+        purus. Nunc mattis lectus quam, sit amet pellentesque ligula vehicula eu. Interdum et malesuada fames ac ante
+        ipsum primis in faucibus. Nunc hendrerit elit a auctor mollis. Cras ac tempor metus. Fusce enim nisl, ornare in
+        odio sit amet, facilisis ornare velit.
+    </p>
+
+    <p>
+        Pellentesque convallis mollis ex, ac suscipit nibh porttitor eget. Morbi vitae mauris et orci dignissim
+        dignissim in quis dolor. Morbi nibh libero, ultrices ut purus quis, fermentum dapibus felis. Vivamus odio
+        ligula, suscipit at ante venenatis, posuere faucibus elit. In luctus elit vel maximus tincidunt. In blandit ex
+        sit amet libero vehicula ornare. Fusce sodales ipsum eget congue gravida. Nullam fringilla, diam ac condimentum
+        dignissim, turpis tortor egestas nisi, ultricies porttitor augue odio sit amet mi. Praesent pellentesque elit
+        nec mi volutpat euismod. Fusce venenatis, augue vitae dapibus ullamcorper, quam est accumsan augue, et tristique
+        orci leo ac dolor. Suspendisse vel lacinia urna. Donec laoreet arcu non nisi ullamcorper mollis. Aliquam
+        placerat sit amet libero a ullamcorper. Proin at ipsum eleifend sapien posuere rutrum posuere non est.
+    </p>
+    
+    <p>
+        Pellentesque convallis mollis ex, ac suscipit nibh porttitor eget. Morbi vitae mauris et orci dignissim
+        dignissim in quis dolor. Morbi nibh libero, ultrices ut purus quis, fermentum dapibus felis. Vivamus odio
+        ligula, suscipit at ante venenatis, posuere faucibus elit. In luctus elit vel maximus tincidunt. In blandit ex
+        sit amet libero vehicula ornare. Fusce sodales ipsum eget congue gravida. Nullam fringilla, diam ac condimentum
+        dignissim, turpis tortor egestas nisi, ultricies porttitor augue odio sit amet mi. Praesent pellentesque elit
+        nec mi volutpat euismod. Fusce venenatis, augue vitae dapibus ullamcorper, quam est accumsan augue, et tristique
+        orci leo ac dolor. Suspendisse vel lacinia urna. Donec laoreet arcu non nisi ullamcorper mollis. Aliquam
+        placerat sit amet libero a ullamcorper. Proin at ipsum eleifend sapien posuere rutrum posuere non est.
+    </p>
+    <p>
+        Pellentesque convallis mollis ex, ac suscipit nibh porttitor eget. Morbi vitae mauris et orci dignissim
+        dignissim in quis dolor. Morbi nibh libero, ultrices ut purus quis, fermentum dapibus felis. Vivamus odio
+        ligula, suscipit at ante venenatis, posuere faucibus elit. In luctus elit vel maximus tincidunt. In blandit ex
+        sit amet libero vehicula ornare. Fusce sodales ipsum eget congue gravida. Nullam fringilla, diam ac condimentum
+        dignissim, turpis tortor egestas nisi, ultricies porttitor augue odio sit amet mi. Praesent pellentesque elit
+        nec mi volutpat euismod. Fusce venenatis, augue vitae dapibus ullamcorper, quam est accumsan augue, et tristique
+        orci leo ac dolor. Suspendisse vel lacinia urna. Donec laoreet arcu non nisi ullamcorper mollis. Aliquam
+        placerat sit amet libero a ullamcorper. Proin at ipsum eleifend sapien posuere rutrum posuere non est.
+    </p>
+
+</div>
+
+    <!-- <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugiat necessitatibus obcaecati libero ratione, esse doloribus beatae aperiam iusto architecto adipisci incidunt porro eum praesentium recusandae, at odit ipsam? Cumque, perspiciatis?</p><p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugiat necessitatibus obcaecati libero ratione, esse doloribus beatae aperiam iusto architecto adipisci incidunt porro eum praesentium recusandae, at odit ipsam? Cumque, perspiciatis?</p> -->
+  <!--          <hgroup>  Relacionar los H y sumarle más importancia en el CEO 
+            <h1>hola</h1>
+            <h2>hola</h2>
+            <h3>hola</h3>
+        </hgroup>  -->
+  
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-122674565-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-122674565-1');
+</script>
+
+<script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+<script src="js/nav.jquery.min.js"></script> 
+<script src="js/main.js"></script>
+<script>
+    $('.nav').nav();
+</script>
+
+</body>
+</html>
+
--- js/main.js
+++ js/main.js
@@ -1,35 +1,25 @@
-$(document).ready(function(){
+$(document).ready(main);
     var x = false;
     var scroll;
 
-   var y = window.matchMedia("(max-width: 949px)")
-   myFunction(y) // Call listener function at run time
-   y.addListener(myFunction) // Attach listener function on state changes
+function main() {
 
-function myFunction() {
-    if (y.matches) { // If media query matches
     $(window).scroll(function(){
         scroll = $(window).scrollTop();
-        if(scroll > 44.67){
+        if(scroll > 45){
             if(!x){    
-                $("h1#header-phone").css({"position":"fixed","width":"100%","z-index":"1000","top":"0px"});
-                $("h1#header-phone div img").css({"margin-top":"4px"});
-                $(".banner").css({"padding-top":"80px"});
+                $("div.master").css({"position":"fixed","top":"0px"});
                 x=true;
             }
-        }else if (scroll < 44.67 ){
+        }else if (scroll < 45 ){
             if(x){
-                $("#header-phone").css({"position": "relative"});
-                $(".banner").css({"padding-top":"0px"});
+                $("div.master").css({"position":"absolute","top":"45px"});
+              
                 x = false;
             }
         } 
     });
-    } else {
-        $(".banner").css({"padding-top":"0px"});
-    }
 }
-});
 
 
 
