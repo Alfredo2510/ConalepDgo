380e3aff1d46e5189765a6f539292aae9c29037d
Alfredo.nava <alfredo_v_n@hotmail.com>
2018-08-23 00:40:05 -0500

    imagen

---
+++ Sections/Inicio/carousel.php
@@ -0,0 +1,50 @@
+
+<div class="slider">
+<div class="container">
+  <div id="myCarousel" class="carousel slide" data-ride="carousel">
+    <!-- Indicators -->
+    <ol class="carousel-indicators">
+      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+      <li data-target="#myCarousel" data-slide-to="1"></li>
+      <li data-target="#myCarousel" data-slide-to="2"></li>
+     
+    </ol>
+
+    <!-- Wrapper for slides -->
+    <div class="carousel-inner">
+
+      <div class="item active">
+        <img src="img/chava3.jpg" alt="Conoce Más">
+        <div class="carousel-caption">
+          <h3>Comienza a construir tu futuro.</h3>
+          <button type="button" class="btn btn-info">Conoce Más...</button>
+        </div>
+      </div>
+      <div class="item">
+        <img src="img/chava3.jpg"  alt="CONALEP">
+        <div class="carousel-caption">
+          <h3>Conalep es tu mejor opción.</h3>
+          <button type="button" class="btn btn-info">Oferta Educativa</button>
+        </div>
+      </div>
+      <div class="item">
+        <img src="img/chava3.jpg"  alt="CONALEP">
+        <div class="carousel-caption">
+          <h3>Forma parte de nuestra familia.</h3>
+          <!--<button type="button" class="btn btn-info">Oferta Educativa</button>-->
+        </div>
+      </div>
+    </div>
+
+    <!-- Left and right controls -->
+    <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+      <span class="glyphicon glyphicon-chevron-left"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="right carousel-control" href="#myCarousel" data-slide="next">
+      <span class="glyphicon glyphicon-chevron-right"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  </div>
+</div>
+</div>
