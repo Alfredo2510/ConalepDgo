0e2b337891e2e51d03fe65a78b72f2107ba09fee
Alfredo.nava <alfredo_v_n@hotmail.com>
2018-08-23 00:38:21 -0500

    scripts para carousel

--- footer.php
+++ footer.php
@@ -11,6 +11,8 @@
 
 <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 <script src="js/nav.jquery.min.js"></script> 
 <script src="js/main.js"></script>
 <script>
