4e6d5a279b1bf98f3c4cb8a7105a09fde3b46f17
Alfredo.nava <alfredo_v_n@hotmail.com>
2018-08-25 14:39:32 -0500

    query para multislider

--- footer.php
+++ footer.php
@@ -9,12 +9,23 @@
   gtag('config', 'UA-122674565-1');
 </script>
 
-<script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+<script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-<script src="js/nav.jquery.min.js"></script> 
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>  
+
 <script src="js/main.js"></script>
+<script src="js/nav.jquery.min.js"></script> 
+
+<script src="js/librery/multislider.min.js"></script> 
+
+<script>
+		$('#mixedSlider').multislider({
+			duration: 1000,
+			interval: 3000
+		});
+</script>
+
 <script>
     $('.nav').nav();
 </script>
