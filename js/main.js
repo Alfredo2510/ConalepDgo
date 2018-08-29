65712a76a8bfabad2e3e26c368a26f5266b32054
Alfredo.nava <alfredo_v_n@hotmail.com>
2018-08-29 05:44:19 -0500

    para el cambio de color del nav

--- js/main.js
+++ js/main.js
@@ -8,17 +8,31 @@ function main() {
         scroll = $(window).scrollTop();
         if(scroll > 45){
             if(!x){    
-                $("div.master").css({"position":"fixed","top":"0px"});
+                $("div.master").css({"position":"fixed","top":"0px","background":"#ffffffe3"});
                 x=true;
             }
         }else if (scroll < 45 ){
             if(x){
-                $("div.master").css({"position":"absolute","top":"45px"});
-              
+                $("div.master").css({"position":"absolute","top":"45px","background":"#ffffff9c"});
                 x = false;
             }
         } 
     });
+
+    $('.ir-arriba').click(function(){
+		$('body, html').animate({
+			scrollTop: '0px'
+		}, 300);
+	});
+ 
+	$(window).scroll(function(){
+		if( $(this).scrollTop() > 0 ){
+			$('.ir-arriba').slideDown(300);
+		} else {
+			$('.ir-arriba').slideUp(300);
+		}
+	});
+
 }
 
 
