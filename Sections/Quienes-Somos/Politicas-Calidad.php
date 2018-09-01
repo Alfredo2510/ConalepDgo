d5f340275c32d3ef8966c4af8b1f31a583c573df
Alfredo.nava <alfredo_v_n@hotmail.com>
2018-09-01 07:18:55 -0500

    seccion politicas

---
+++ Sections/Quienes-Somos/Politicas-Calidad.php
@@ -0,0 +1,12 @@
+<?php include '../../header_footer/header.php';?> 
+
+
+<h2 class="general">Políticas de Calidad</h2>
+<div class="imagen_quienes_somos MV">
+    IMAGEN
+</div>
+<p class="general">
+Quienes integramos el Colegio de Educación Profesional Técnica, tenemos el compromiso de realizar un trabajo profesional equitativo, pertinente e incluyente; sustentado en los valores institucionales, cumpliendo con los requerimientos de nuestros clientes, así como los legales aplicables, mejorando continuamente la efectividad de nuestros procesos y cumpliendo con el Sistema de Gestión de  Calidad con apego a la norma ISO 9001:2008.
+</p>
+
+<?php include '../../header_footer/footer.php';?> 
