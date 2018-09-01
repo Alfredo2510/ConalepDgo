93f08d6581dcb3f53d3dd221c6e0b444e7a62df0
Alfredo.nava <alfredo_v_n@hotmail.com>
2018-09-01 07:19:23 -0500

    seccion directorio

---
+++ Sections/Quienes-Somos/Directorio.php
@@ -0,0 +1,26 @@
+<?php include '../../header_footer/header.php';?> 
+
+<h2 class="general">Directorio</h2>
+
+<div class="directorio-tabla">
+    <table class="table table-striped">
+        <thead>
+        <tr>
+            <th>UNIDAD</th>
+            <th>DIRECCIÓN</th>
+            <th>TELÉFONOS</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+            <td>Plantel Durango</td>
+            <td>Calle Armando del Castillo Franco S/N Fracc. Fidel Velázquez Gómez Palacio, Dgo. CP 35025</td>
+            <td>(871) <br>
+                719-60-26
+                719-62-50
+            </td>
+        </tr>
+        </tbody>
+    </table>
+</div>
+<?php include '../../header_footer/footer.php';?> 
