20fe303129317a2969b1e2ab81f808d4db427f7b
Alfredo.nava <alfredo_v_n@hotmail.com>
2018-09-01 07:19:08 -0500

    seccion mision-vision

---
+++ Sections/Quienes-Somos/Mision-Vision.php
@@ -0,0 +1,15 @@
+<?php include '../../header_footer/header.php';?> 
+
+<h2 class="general">Misión y Visión</h2>
+<div class="imagen_quienes_somos MV">
+    IMAGEN
+</div>
+<span class="general-MV">Nuestra Misión</span>
+<p class="general">
+Formar Profesionales Técnicos - Bachilleres, a través de un Modelo Académico de Calidad para la  Competitividad, mediante una modalidad escolarizada y mixta; otorgamos servicios  de capacitación y evaluación en competencias laborales, manteniendo una estrecha vinculación con los sectores productivos.
+</p>
+<span class="general-MV">Nuestra Visión</span>
+<p class="general">
+Somos líderes en la formación de Profesionales Técnicos Bachilleres , en la capacitación y evaluación de competencias laborales, respondiendo efectivamente a las necesidades de nuestros clientes.
+</p>
+<?php include '../../header_footer/footer.php';?> 
