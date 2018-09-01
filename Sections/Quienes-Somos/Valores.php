ad3510914085c520cf56a70f9b78f8cf69b58f8f
Alfredo.nava <alfredo_v_n@hotmail.com>
2018-09-01 07:18:27 -0500

    se agrego seccion valores

---
+++ Sections/Quienes-Somos/Valores.php
@@ -0,0 +1,87 @@
+<?php include '../../header_footer/header.php';?> 
+
+<h2 class="general">Nuestros Valores</h2>
+
+<div class="Valores-I">
+    <div class="img-V">
+        <img src="../../img/Calidad.jpg" alt="Calidad">
+    </div>
+    <div class="letras-V">
+        <h4>C A L I D A D</h4>
+        <h5>"La primera es la vencida"</h5>
+        <p class="general PV">Hacemos las cosas bien desde la primera vez, teniendo en mente a la persona o área que hará uso de nuestros productos o servicios, considerando lo que necesita y cuándo lo necesita.</p>
+    </div>
+</div>
+<hr>
+<div class="Valores-D">
+    <div class="letras-V">
+        <h4>C O M U N I C A C I Ó N</h4>
+        <h5>"Información para el entendimiento, comunicación para el aprendizaje"</h5>
+        <p class="general PV">Fomentamos la fluidez de comunicación institucional, lo que implica claridad en la transmisión de ideas y de información, así como una actitud responsable por parte del receptor.</p>
+    </div>
+    <div class="img-V">
+        <img src="../../img/Comunicacion.jpg" alt="Comunicacion">
+    </div>
+</div>
+<hr>
+<div class="Valores-I">
+    <div class="img-V">
+        <img src="../../img/Mentalidad.jpg" alt="Mentalidad Positiva">
+    </div>
+    <div class="letras-V">
+        <h4>M E N T A L I D A D</h4>
+        <h4>P O S I T I V A</h4>
+        <h5>"El camino más largo empieza con el primer paso"</h5>
+        <p class="general PV">Tenemos la disposición para enfrentar retos con una visión de éxito, considerando que siempre habrá una solución para cada problema y evitando la inmovilidad ante la magnitud de la tarea a emprender.</p>
+    </div>
+</div>
+<hr>
+<div class="Valores-D">
+    <div class="letras-V">
+        <h4>R E S P O N S A B I L I D A D</h4>
+        <h5>"No esperes a que otros lo hagan por ti"</h5>
+        <p class="general PV">Cada uno de nosotros debe responsabilizarse del resultado de su trabajo y tomar sus propias decisiones dentro del ámbito de su competencia.</p>
+    </div>
+    <div class="img-V">
+        <img src="../../img/Responsabilidad.jpg" alt="Responsabilidad">
+    </div>
+</div>
+<hr>
+<div class="Valores-I">
+    <div class="img-V">
+        <img src="../../img/Cooperacion.jpg" alt="Cooperacion">
+    </div>
+    <div class="letras-V">
+        <h4>C O O P E R A C I Ó N</h4>
+        <h5>"Juntos lo haremos mejor"</h5>
+        <p class="general PV">El todo es más que las suma de las partes, por lo que impulsamos el trabajo en equipo, respetando las diferencias, complementando esfuerzos y construyendo aportaciones de los demás.</p>
+    </div>
+</div>
+<hr>
+<div class="Valores-D">
+    <div class="letras-V">
+        <h4>C O M P R O M I S O</h4>
+        <h4>C O N  &nbsp;&nbsp;  L A</h4>
+        <h4>S O C I E D A D</h4>
+        <h5>"La Sociedad: Horizonte de nuestros esfuerzos"</h5>
+        <p class="general PV">Reconocemos a la sociedad como la beneficiaria de nuestro trabajo, considerando la importancia de su participación en la determinación de nuestro rumbo. Para ello debemos atender las necesidades específicas de cada región, aprovechando las ventajas y compensando las desventajas en cada una de ellas.</p>
+    </div>
+    <div class="img-V">
+        <img src="../../img/Compromiso.jpg" alt="Compromiso">
+    </div>
+</div>
+<hr>
+<div class="Valores-I">
+    <div class="img-V">
+        <img src="../../img/Respeto.jpg" alt="Respeto">
+    </div>
+    <div class="letras-V">
+        <h4>R E S P E T O</h4>
+        <h4>A  &nbsp; L A</h4>
+        <h4>P E R S O N A</h4>
+        <h5>"Respetar al otro es respetarte a ti mismo"</h5>
+        <p class="general PV">Consideramos a cada una de las personas como individuos dignos de atención, con intereses más allá de lo estrictamente profesional o laboral.</p>
+    </div>
+</div>
+
+<?php include '../../header_footer/footer.php';?> 
