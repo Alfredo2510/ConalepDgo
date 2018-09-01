dfc29c3bd6e99325d284f0fdce4074471fe7d95a
Alfredo.nava <alfredo_v_n@hotmail.com>
2018-09-01 07:21:11 -0500

    se agrego otro header aparte del de index

---
+++ header_footer/header.php
@@ -0,0 +1,132 @@
+<!DOCTYPE html>
+<html lang="es-mx" class="nav-no-js">
+<head> <!-- Información de la pagina para el navegador y también para enlazar archivos (css, js, entre otros) -->
+    <meta charset="UTF-8"> <!-- Decodificación de los caracteres -->
+    <title>CONALEP Durango</title>
+    <meta name="description" content="Colegio de Educación Profesional Técnica del Estado de Durango
+                                     | Portal Conalep">
+    <meta name="author" content="https://plus.google.com/109717154884126540058">
+    <meta name="owner" content="CONALEP Durango">
+    <meta name="viewport" content="width=device-width, user-scalabe=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+  
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="http://netdna.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <link rel="stylesheet" href="../../css/fonts.css">
+    <link rel="stylesheet" href="../../css/normalize.css">
+
+
+
+    <link rel="stylesheet" href="../../css/style index/nav-core.css">
+    <link rel="stylesheet" href="../../css/style index/nav-layout.css">
+    <link rel="stylesheet" href="../../css/style index/estilo-index.css">
+    <link rel="stylesheet" href="../../css/inicio/style-footer.css">
+    <link rel="stylesheet" href="../../css/Somos/Quienes-somos.css">
+   
+
+    <script src="../../js/librery/modernizr-custom.js"></script>
+    <script src="../../js/librery/all.min.js"></script>
+</head>
+
+<body> 
+        <div id="up-header">
+                <a class="info-up" id="tel-head" href="tel:+52-618-819-9626"> <span><i class="fas fa-phone icon-up"></i>(618) 819-96-26 y 817-12-68</span></a>
+                <a class="info-up" target="__blank" href="https://www.google.com.mx/maps/dir/''/conalep+durango/@24.0692589,-104.6045421,18.05z/data=!4m8!4m7!1m0!1m5!1m1!1s0x869bb6f1733f6e9f:0x1758bea35d7db179!2m2!1d-104.6038647!2d24.0698149"><span class="info-up"><i class="fas fa-map-marker-alt icon-up"></i>Selenio Lote 1, Cd Industrial, 34208 Durango, Dgo.</span></a> 
+                <a class="info-up" id="mail-head" href="mailto:direccionconalepdgo@gmail.com"> <span><i class="fas fa-envelope icon-up"></i>direccionconalepdgo@gmail.com</span></a>
+            <div id="red-icons">
+                <a target="__blank" href="https://www.facebook.com/conalep.planteldurango/"><i class="i-fb fab fa-facebook icon-upfb"></i></a> 
+                <a target="__blank" href="https://twitter.com/conalep130?lang=es"><i class="fab fa-twitter-square icon-uptw"></i></a> 
+            </div>
+        </div>
+
+<header>
+        <div class="phone-c">
+            <a href="#"><img src="../../img/logo.png" alt="Conalep Durango"></a>
+        </div>
+</header>
+
+<a href="#" class="nav-button">Menu</a>
+     
+        <div class="master">   
+        <h1>
+            <div>
+            <a href="../../index.php"><img src="../../img/logo.png" alt="Conalep Durango"></a>
+            </div>
+        </h1> 
+                    <nav class="nav" > <!-- Menú de navegación -->
+                            <ul> <!-- Una lista no ordenada, para contener los elementos del menú -->
+                                <li ><a href="../../index.php" ><i class="fas fa-home"></i></a></li> 
+
+                                <li  class="nav-submenu">
+                                    <a href="../../Sections/Quienes-Somos/Quienes-Somos.php" >QUIÉNES SOMOS</a>
+                                    <ul>
+                                        <li><a href="../../Sections/Quienes-Somos/Mision-Vision.php">Misión y Visión</a></li>
+                                        <li><a href="../../Sections/Quienes-Somos/Valores.php">Nuestros Valores</a></li>
+                                        <li><a href="../../Sections/Quienes-Somos/Politicas-Calidad.php">Políticas de Calidad</a></li>
+                                        <li><a href="../../Sections/Quienes-Somos/Directorio.php">Directorio</a></li>
+                                    </ul>
+                                </li>
+                                <li class="nav-submenu">
+                                    <a href="../../Sections/Alumnos/Alumnos.php" >ALUMNOS</a>
+                                    <ul>
+                                        <li><a href="#">Oferta Educativa</a></li>
+                                        <li><a href="#">Red Académica</a></li>
+                                        <li class="nav-submenu"><a href="#">Becas Conalep</a>
+                                            <ul>
+                                                <li><a href="#">Convocatorias</a></li>
+                                                <li><a href="#">Padrones</a></li>
+                                                <li><a href="#">Contraloria Social</a></li>
+                                            </ul>
+                                        </li>
+                                        <li><a href="#">Valoracion de Aprovechamiento</a></li>
+                                        <li><a href="#">Becas Externas y Estímulos</a></li>
+                                        <li><a href="#">Convocatorias</a></li>
+                                    </ul>
+                                </li> <!-- Manda a un identificador marcado con la misma id  -->
+                                <li class="nav-submenu">
+                                    <a href="#">DOCENTES</a>
+                                    <ul>
+                                        <li><a href="#">Programa "Evaluación Integral"</a></li>
+                                        <li><a href="#">Formación Docente</a></li>
+                                        <li><a href="#">SEMS-COSDAC</a></li>
+                                        <li><a href="#">Convocatorias</a></li>
+                                    </ul>
+                                </li>
+                                <li class="nav-submenu">
+                                    <a href="#">VINCULACIÓN</a>
+                                    <ul>
+                                        <li><a href="#">Encuestas</a></li>
+                                        <li><a href="#">Colocación de Egresados</a></li>
+                                        <li><a href="#">Bolsa de Trabajo</a></li>
+                                        <li><a href="#">Comités de Vinculación</a></li>
+                                    </ul>
+                                </li>
+                                <li class="nav-submenu">
+                                    <a href="#">SERVICIOS</a>
+                                    <ul>
+                                        <li><a href="#">Capacitación</a></li>
+                                        <li><a href="#">Centros de Evaluación</a></li>
+                                        <li><a href="#">Atención a la Comunidad</a></li>
+                                    </ul>
+                                </li>
+                                <li class="nav-submenu">
+                                    <a href="#">TRANSPARENCIA</a>
+                                    <ul>
+                                        <li><a href="#">Aviso de Privacidad</a></li>
+                                        <li><a href="#">Cumplimiento Art 65</a></li>
+                                        <li><a href="#">Cumplimiento Art 75 LGCG</a></li>
+                                        <li><a href="#">Transparencia Presupuestal</a></li>
+                                    </ul>
+                                </li>
+                                
+                                <li> <a class="mn-mother" href="#">CONTACTO</a> </li>
+                            </ul>
+                    </nav>
+            </div>   
+            <div class="espacio"></div>
+
+  
+
+
