bdca8567ce76b3d15dd3530c2e23b1c03cc68c55
Alfredo.nava <alfredo_v_n@hotmail.com>
2018-09-01 07:18:43 -0500

    seccion quienes somos

---
+++ Sections/Quienes-Somos/Quienes-Somos.php
@@ -0,0 +1,12 @@
+<?php include '../../header_footer/header.php';?> 
+
+
+<h2 class="general">Quiénes Somos</h2>
+<div class="imagen_quienes_somos">
+    IMAGEN
+</div>
+<p class="general">
+Somos una institución de Educación Profesional Técnica en el estado de Durango, dedicada a la formación de Profesionales Técnicos Bachilleres  de nivel medio superior, a prestar servicios  de  capacitación   laboral  basada en normas de competencia y a la atención de zonas marginadas. Todo esto con la finalidad de satisfacer la demanda y necesidades del sector productivo, público y social en la entidad, buscando el desarrollo local, regional y estatal, todo en un marco de calidad, pertinencia y equidad.
+</p>
+
+<?php include '../../header_footer/footer.php';?> 
