8b80f1e984d68fe64d369f50153aa737186f5546
Alfredo.nava <alfredo_v_n@hotmail.com>
2018-09-10 09:45:52 -0500

    nueva seccion

---
+++ Sections/Alumnos/Valoracion-Aprovechamiento.php
@@ -0,0 +1,19 @@
+<?php include '../../header_footer/header.php';?> 
+
+<h2 class="general">Valoración del Aprovechamiento Académico </h2>
+<p class="general">Como una acción preventiva para disminuir la reprobación y abandono escolar, en CONALEP se realiza el seguimiento al aprovechamiento académico, mediante el Sistema de Administración Escolar (SAE), mediante cortes semestrales de información de los alumnos respecto de sus evaluaciones y asistencias a la 6ª,11ª y 16ª semanas de haber iniciado el ciclo semestral, con ello se puede identificar a los alumnos que están en riesgo de abandono escolar, así como que los docentes del Sistema registren las evaluaciones y asistencias de sus alumnos en tiempo, lo que permite dar un seguimiento personalizado por alumno y por ende establecer estrategias de seguimiento y retención de los mismos.</p>
+
+<div class="accordion-up">
+    <button class="accordion">Documentos.</button>
+    <div class="panel">
+        <div id="red-academica">
+            <div>
+                <a href="../../pdf/BECAS/Gu_a_pr_ctica_para_el_Seguimiento_al_aprovechamiento_Acad_mico.pdf" target="blank"><i class="far fa-file-pdf"></i>•	Guía práctica para el Seguimiento al aprovechamiento Académico.</a>
+                <a href="../../pdf/BECAS/Boletin_Contraloria Social_2.pdf" target="blank"><i class="far fa-file-pdf"></i>•	Boletín de Contraloría Social.</a>
+                <a href="../../pdf/BECAS/Díptico_2_Contraloria_Social.pdf" target="blank"><i class="far fa-file-pdf"></i>•	Díptico de Contraloría Social.</a>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php include '../../header_footer/footer.php';?> 
